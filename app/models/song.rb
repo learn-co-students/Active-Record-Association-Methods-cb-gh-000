335dd711c889245548ca11096c896635c13150be
robertjosephwayne <robertjwayne@gmail.com>
2017-04-26 11:13:50 -0400

    Create #drake_made_this method for Song

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,8 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
+    drake = Artist.find_or_create_by(name: "Drake")
+    drake.songs << self
+    drake
   end
 end
